fc9c4bf673bb222828fcebf464e18a01cf960ecc
kiranyak <56093121+kiranyak@users.noreply.github.com>
2020-05-31 13:19:28 +0200

    Update DecisionTree_VegetationClassification.py
    edited

--- DecisionTree_VegetationClassification.py
+++ DecisionTree_VegetationClassification.py
@@ -1,23 +1,4 @@
-""""
- In this context, this work developed an original method to detect in near-real-time the onset of vegetation senescence. The design of the detection relies on the temporal
-behavior of two indices: the Normalized Difference Vegetation Index, depending on the
-green vegetation, and the Normalized Difference Tillage Index, sensitive to both green
-and dry vegetation. The method is demonstrated in Mauritania, an ever-affected country,
-with 10-day MODIS mean composites for the years 2010 and 2011. The discrimination
-performance of three classes (“growth”, “density reduction” and “drying”) were analyzed for
-three classification methods: maximum likelihood (61.4% of overall accuracy), decision tree
-(71.5%) and support vector machine (72.3%). The classification accuracy is heterogeneous
-in both time and space and is affected by several factors, such as vegetation density, the
-north-south climatic gradient and the relief. Smoothing the vegetation time series resulted
-in an increase of the overall accuracy of about 5% at the expense of a loss in timeliness of
-ten days. To simulate near-real-time monitoring conditions, the decision tree was applied to
-the decade of 2010. Overall, the seasonal vegetation cycle appeared clear and consistent.
-The results obtained pave the way for an operational implementation of the senescence
-dynamic mapping and, consequently, to further strengthen the capacity of the locust
-control management.
 
-
-""""
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
